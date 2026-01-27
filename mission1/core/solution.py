9741bf84e010b8a39150fd7aa9af086b135902db
moeez65890 <rbm9z2s5gg@privaterelay.appleid.com>
2026-01-27 15:58:21 +0100

    Update solution.py

--- mission1/core/solution.py
+++ mission1/core/solution.py
@@ -1,1 +1,20 @@
+# Mission 1 – Main Exercise
+# Find solutions to: a * x + b * y = c
 
+a = 2
+b = 3
+c = 13
+max_value = 10
+
+solutions = 0
+
+for x in range(1, max_value + 1):
+    for y in range(1, max_value + 1):
+        if a * x + b * y == c:
+            print("x =", x, ", y =", y)
+            solutions += 1
+
+if solutions == 0:
+    print("No solution found")
+else:
+    print(solutions, "solutions found")
